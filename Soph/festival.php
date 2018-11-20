8112ee6c1afa5f234dbefa8316816202a51d2c78
Sophie Zhang <syt.zhang@gmail.com>
2018-11-20 12:14:03 -0500

    added php to create cards for each film, created festival page to display festival information

--- Soph/festival.php
+++ Soph/festival.php
@@ -1,11 +1,6 @@
-<?php 
 
-?>
 <!DOCTYPE html>
 <head>
-	<!-- Style sheet -->
-	<link rel="stylesheet" type="text/css" href="browse_page.css">
-
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -28,7 +23,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-	<div class="container-fluid">
+	<div class="container-fluid" style="padding-left: 0px; padding-right: 0px">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<!-- Brand -->
 			<a class="navbar-brand" href="#">FilmFest</a>
@@ -36,11 +31,11 @@
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
   			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
+					<li class="nav-item active">
 				        <a class="nav-link" href="/">Home</a>
 			      	</li>
-			      	<li class="nav-item active">
-			        	<a class="nav-link" href="browse_festivals.php">Browse Festivals<span class="sr-only">(current)</span></a>
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="browse_festivals.php">Browse Festivals</a>
 			      	</li>
 			      	<li class="nav-item">
 			      		<a class="nav-link" href="#">About</a>
@@ -53,45 +48,25 @@
 		</nav>
 	</div>
 	<div class="container">
-		<form action="#" method="get">
-			<div class="row search" style="margin-top: 20px">
-				<div class="col-8">
-					<input autocomplete="on" size="80" class="form-control">
-				</div>
-				<div class="col-2">
-					<button type="button" class="btn btn-info">Search</button>
-				</div>
-			</div>
-		</form>
-		<hr>
 		<div class="row">
 			<div class="col">
-				<?php
-					// include "search.php";
-				?>
-				<div class="card">
-					<!-- <span style="display:inline-block;"> -->
-					<div class="row">
-						<div class="col-s-6 col-md-4">
-							<img src="https://storage.googleapis.com/ff-storage-p01/festivals/logos/000/001/589/large/logo.jpg?1537117981" style="height: 200px; width: 200px;">
-						</div>
-						<div class="col film-card">
-							<h5 class="card-title">Festival Name</h5>
-							<p class="card-text">Short description</p>
-							<div style="position:absolute; bottom:20px;"><a class="btn btn-secondary" href="festival.php?id=1">Details</a></div>
-						</div>
+				<div style="margin-top: 20px">
+					<?php 
+						// include "connect_db.php";
+						if (is_null($_GET['id']) || $_GET['id'] == '') {
+							echo "Festival not found!";
+							exit();
+						}
+
+						// Get the film information to display 
+						$name = "Festival Name";
+
+
+					?>
+					<h3><?php echo $name;?></h3>
 				</div>
-					<!-- </span> -->
-					<!-- <div class="card-body" style="display:inline-block;">
-						<h5 class="card-title">Festival Name</h5>
-						<p class="card-text">Short description</p>
-						<a href="#" class="btn btn-secondary">See details</a>
-					</div> -->
-				</div>
+				<hr>
 			</div>
 		</div>
 	</div>
-<body>
 </body>
-</html>
-
--- Soph/search.php
+++ Soph/search.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	// $conn
-	include "connect_db.php";
+	// include "connect_db.php";
 
 	// Check what user searched
 	if (isset($_POST['input']) && $_POST['input'] != "") {
@@ -16,9 +16,25 @@
 
 		$films = $result->fetch_assoc();
 		foreach ($films as $film) {
+			$name = $film[''];					// Fill column name
+			$descr = $film['about']; 	// Fill column name
+			if (strlen($descr) > 350) {
+				$descr = substr($descr, 0, strrpos($descr, " "));
+				$descr .= "...";
+			}
+			$pic = $film['icon']; 				// Fill column name
+
+			echo "<div class='card'><div class='row'><div class='col-s-6 col-md-4'>";
+			echo "<img sr='" . $pic . "' style='height: 200px; width: 200px;'></div>";
+			echo "<div class='col film-card'>";
+			echo "<h5 class='card-title'>" . $name . "</h5>";
+			echo "<p class='card-text'>" . $descr . "</p>";
+			echo "<div style='position:absolute; bottom:20px;'><a class='btn btn-secondary' href='festival.php?id=" . $film['id_festival'] . "'>Details</a></div></div></div>";
 
 		}
 	}
+
+
 
 	
 
